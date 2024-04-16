54f727b49793ab0bcfe4f80f54e6207a49ec312c
ruteru <garcicon45@gmail.com>
2024-04-16 11:46:40 -0300

    logos using javascript

---
+++ src/components/logosBool.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './logos.css';
+
+class Logos extends React.Component {
+  render() {
+    const { logosData, numSlides } = this.props;
+
+    const filteredLogosData = Object.entries(logosData.favoriteApps)
+      .filter(([key, value]) => value[0] === true)
+      .map(([key, value]) => ({ alt: key, src: value[1].img }));
+
+    const logosSlides = [];
+    for (let i = 0; i < numSlides; i++) {
+      logosSlides.push(
+        <div className="logos-slide" key={i}>
+          {filteredLogosData.map((logo, index) => (
+            <img alt={logo.alt} src={logo.src} key={index} />
+          ))}
+        </div>
+      );
+    }
+
+    return <div className="logos">{logosSlides}</div>;
+  }
+}
+
+export default Logos;
