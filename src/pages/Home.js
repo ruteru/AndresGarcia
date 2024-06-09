2168a65a4173a582f538e83b4c221790d0d66ab2
ruteru <garcicon45@gmail.com>
2024-06-08 21:30:14 -0300

    move context provider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,16 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import Body from "../components/Body/Body";
-import Footer from "../components/Footer/Footer";
 import Presentation from '../components/Body/presentationSection/Presentation'
 import '../styles/home.css';
-import { IconContext } from "react-icons";
 
 export default function Home() {
     return (
         <div className="container-home">
-            <IconContext.Provider value={{ color: '#1e1e20', size: '1.8em', className: 'icon-context' }}>
-                <Header />
-                <Body>
-                    <Presentation />
-                </Body>
-                <Footer />
-            </IconContext.Provider>
+            <Header />
+            <Body>
+                <Presentation />
+            </Body>
         </div>
     );
 }
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,19 +1,15 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import Body from "../components/Body/Body";
-import Footer from "../components/Footer/Footer";
-import SectionsServices from "../components/Body/services/SectionsServices";
-import { IconContext } from "react-icons";
+import SectionsServices from "../components/Body/serviceSection/SectionsServices";
 
 export default function Services() {
     return(
         <div>
-            <IconContext.Provider value={{ color: '#1e1e20', size: '1.8em', className: 'icon-context' }}>
                 <Header />
                 <Body>
                     <SectionsServices/>
                 </Body>
-            </IconContext.Provider>
         </div>
     );
 }
