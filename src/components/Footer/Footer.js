76cd20794268fa378ca92ab23dc3b9eb286b8766
ruteru <garcicon45@gmail.com>
2024-06-08 21:32:28 -0300

    remove footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,10 @@
 import React from "react";
-import '../Footer/footer.css';
+import './footer.css';
 
 export default function Footer() {
     return (
         <div className="container-footer">
-            <span>hello</span>
+            <span>footer</span>
         </div>
     );
 }
