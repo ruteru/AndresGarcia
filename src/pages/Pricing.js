cecf92b72a0604687e8e2ba308ae24d3a19f8c7b
ruteru <garcicon45@gmail.com>
2024-06-10 11:31:54 -0300

    fix undefined elements

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,16 +1,11 @@
 import React from "react";
 import Header from "../components/Header/Header";
-import Body from "../components/Body/Body";
-import Footer from "../components/Footer/Footer";
 
 export default function Pricing() {
     return (
         <div>
-                <Header />
-                <Body>
-                    <h1>Pricing Page</h1>
-                </Body>
-                <Footer />
+            <Header />
+            <h1>Pricing Page</h1>
         </div>
     );
 }
