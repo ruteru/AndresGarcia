7a174107dbb931f6c48c5d3506232c56bd25f638
ruteru <garcicon45@gmail.com>
2024-06-10 15:19:01 -0300

    update tasks design

--- src/components/Body/pricing/Tasks.js
+++ src/components/Body/pricing/Tasks.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import Calendar from '../../Calendar/Calendar';
 import './tasks.css';
-import { FcApproval } from "react-icons/fc";
 
 export default function Tasks() {
     const [selectedDates, setSelectedDates] = useState([]);
     const [selectedDevices, setSelectedDevices] = useState([]);
     const [selectedServices, setSelectedServices] = useState([]);
 
-    const devicesOptions = ["Desktop", "Tablet", "Smartphone"];
+    const devicesOptions = ["Desktop", "Smartphone", "Other"];
     const servicesOptions = ["Backend", "Frontend", "Fullstack", "Machine Learning"];
 
     const handleCheckboxChange = (option, setSelected, selected) => {
@@ -20,12 +19,16 @@ export default function Tasks() {
     };
 
     const handleDateSelect = (date) => {
-        const formattedDate = date.toISOString().split('T')[0];
-        if (!selectedDates.includes(formattedDate)) {
-            setSelectedDates([...selectedDates, formattedDate]);
+        const dateString = date.toISOString().split('T')[0];
+        let newSelectedDates = [...selectedDates];
+
+        if (newSelectedDates.length === 2) {
+            newSelectedDates = [newSelectedDates[1], dateString];
         } else {
-            setSelectedDates(selectedDates.filter(d => d !== formattedDate));
+            newSelectedDates.push(dateString);
         }
+
+        setSelectedDates(newSelectedDates);
     };
 
     return (
@@ -33,40 +36,42 @@ export default function Tasks() {
             <div className="optionsSelector">
                 <div className="selectDates">
                     <h4>Select Deadlines</h4>
-                    <Calendar onSelectDate={handleDateSelect} />
+                    <Calendar selectedDates={selectedDates} onSelectDate={handleDateSelect} />
                     <div className="selected-dates">
                         {selectedDates.map(date => (
-                            <div key={date}>{date}</div>
+                            <div key={date}></div>
                         ))}
                     </div>
                 </div>
-                <div className="selectDevices">
-                    <h4>Select Devices</h4>
-                    {devicesOptions.map(device => (
-                        <label key={device}>
-                            <input
-                                type="checkbox"
-                                value={device}
-                                checked={selectedDevices.includes(device)}
-                                onChange={() => handleCheckboxChange(device, setSelectedDevices, selectedDevices)}
-                            />
-                            {device}
-                        </label>
-                    ))}
-                </div>
-                <div className="selectServices">
-                    <h4>Select Service</h4>
-                    {servicesOptions.map(service => (
-                        <label key={service}>
-                            <input
-                                type="checkbox"
-                                value={service}
-                                checked={selectedServices.includes(service)}
-                                onChange={() => handleCheckboxChange(service, setSelectedServices, selectedServices)}
-                            />
-                            {service}
-                        </label>
-                    ))}
+                <div className="listsTasks">
+                    <div className="selectDevices">
+                        <strong>Select Devices</strong>
+                        {devicesOptions.map(device => (
+                            <label key={device}>
+                                <input
+                                    type="checkbox"
+                                    value={device}
+                                    checked={selectedDevices.includes(device)}
+                                    onChange={() => handleCheckboxChange(device, setSelectedDevices, selectedDevices)}
+                                />
+                                {device}
+                            </label>
+                        ))}
+                    </div>
+                    <div className="selectServices">
+                        <strong>Select Service</strong>
+                        {servicesOptions.map(service => (
+                            <label key={service}>
+                                <input
+                                    type="checkbox"
+                                    value={service}
+                                    checked={selectedServices.includes(service)}
+                                    onChange={() => handleCheckboxChange(service, setSelectedServices, selectedServices)}
+                                />
+                                {service}
+                            </label>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
