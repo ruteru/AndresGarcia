75295e1f4df4b758a73116785260c34282536f21
ruteru <garcicon45@gmail.com>
2024-06-10 16:56:06 -0300

    improve responsiveness

--- src/components/Body/workSection/ResumeComponent.js
+++ src/components/Body/workSection/ResumeComponent.js
@@ -8,7 +8,7 @@ export default function ResumeComponent() {
             {resume.work.map((job, index) => (
                 <div key={index} className="work-item">
                     <h3>{job.title} at <a href={job.companyUrl} target="_blank" rel="noopener noreferrer">{job.company}</a></h3>
-                    <p style={{textAlign: 'center'}}>{job.date}</p>
+                    <p>{job.date}</p>
                     <p>{job.details}</p>
                 </div>
             ))}
