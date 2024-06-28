1807f60a147eb7988dcf68aba5ce50de6c53ff6d
ruteru <garcicon45@gmail.com>
2024-06-28 13:50:23 -0300

    remove webscrapper component

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,16 +1,11 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import ResumeComponent from "../components/Body/workSection/ResumeComponent";
-import ExternalData from "../components/Web/ExternalData";
 export default function Work() {
     return (
         <div>
             <Header />
             <ResumeComponent />
-            <div className='dataContainer'>
-                <h1>Client-side Web Scraper</h1>
-                <ExternalData />
-            </div>
         </div>
     );
 }
