ce2b63314526b10cbb07acf3515cc136c3e3d060
ruteru <garcicon45@gmail.com>
2024-06-27 23:53:57 -0300

    remove construction banner replace tasks with fix prices

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,15 +1,14 @@
 import React from "react";
 import Header from "../components/Header/Header";
-import Tasks from "../components/Body/pricing/Tasks";
-import { ConstructionProvider } from '../Context/ConstructionContext';
+import Tasks from "../components/Body/pricing/lib/Tasks";
+import Prices from "../components/Body/pricing/Prices";
 
 export default function Pricing() {
     return (
         <div>
             <Header />
-            <ConstructionProvider>
-                <Tasks />
-            </ConstructionProvider>
+            <Prices />
+            {/* <Tasks /> */}
         </div>
     );
 }
