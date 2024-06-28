ff97407b7e12b4d93797e34b0ba52897e1b470e8
ruteru <garcicon45@gmail.com>
2024-06-28 16:56:16 -0300

    remove unused file

--- updateManifest.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const resumeData = require('./src/data/resume.json');
-const name = resumeData.name;
-const manifestPath = './public/manifest.json';
-const manifest = require(manifestPath);
-
-manifest.short_name = name;
-
-fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
