b380997cf080a1735b1342911f8fa7a1140fbe56
ruteru <garcicon45@gmail.com>
2024-06-28 16:55:46 -0300

    add supress warnings

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/Header/Header";
+// eslint-disable-next-line
 import Tasks from "../components/Body/pricing/lib/Tasks";
 import Prices from "../components/Body/pricing/Prices";
 
